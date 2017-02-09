cc415e50eff215ccce9df1ed97e169242d93c2b3
Dzmitry Chyrta <dima@chyrta.com>
2017-02-09 16:31:34 +0300

    Initial Commit

---
+++ Munchkin Level Counter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Munchkin Level Counter
+//
+//  Created by Dzmitry Chyrta on 09.02.17.
+//  Copyright © 2017 datarockets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
