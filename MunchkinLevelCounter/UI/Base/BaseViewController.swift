fa4fce99364f357b8fab32502429678df3341fb8
Dzmitry Chyrta <dima@chyrta.com>
2017-03-02 16:55:48 +0300

    Created Appearance class to define global styles for UI and BaseViewController to colorize status bar background which is view

---
+++ MunchkinLevelCounter/UI/Base/BaseViewController.swift
@@ -0,0 +1,22 @@
+//
+//  BaseViewController.swift
+//  MunchkinLevelCounter
+//
+//  Created by Dzmitry Chyrta on 02.03.17.
+//  Copyright © 2017 datarockets. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = Colors.cardLight
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
---
+++ MunchkinLevelCounter/Util/Appearance.swift
@@ -0,0 +1,27 @@
+//
+//  Appearance.swift
+//  MunchkinLevelCounter
+//
+//  Created by Dzmitry Chyrta on 01.03.17.
+//  Copyright © 2017 datarockets. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Appearance {
+    
+    static func setupUIAppearance() {
+        // navigation bar color - card light
+        // status bar color - card general
+        // color accent (checkbox and textfields) - card corner
+        
+        
+        UINavigationBar.appearance().barTintColor = Colors.cardLight
+        UINavigationBar.appearance().tintColor = Colors.cardCorner
+        
+        UISwitch.appearance().onTintColor = Colors.cardCorner.withAlphaComponent(0.2)
+        UISwitch.appearance().thumbTintColor = Colors.cardCorner
+    }
+    
+}
