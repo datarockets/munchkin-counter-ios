568865e3b2b325505d428f36674156761a6f623f
Dzmitry Chyrta <dima@chyrta.com>
2017-03-02 16:52:30 +0300

    Added theme colors to Colors

---
+++ MunchkinLevelCounter/Util/Colors.swift
@@ -0,0 +1,18 @@
+//
+//  Colors.swift
+//  MunchkinLevelCounter
+//
+//  Created by Dzmitry Chyrta on 02.03.17.
+//  Copyright © 2017 datarockets. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Colors {
+    static let black = UIColor.black
+    static let cardGeneral = UIColor(red: 0.97, green: 0.77, blue: 0.41, alpha: 1.0)
+    static let cardLight = UIColor(red:0.97, green:0.84, blue:0.45, alpha: 1.0)
+    static let cardCorner = UIColor(red:0.23, green:0.09, blue:0.02, alpha: 1.0)
+    static let cardBackground = UIColor(red:0.97, green:0.91, blue:0.83, alpha:1.0)
+}
