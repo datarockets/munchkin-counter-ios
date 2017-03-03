a95168958926156769b8989752cd9ac5b4e86f9f
Dzmitry Chyrta <dima@chyrta.com>
2017-03-03 15:00:50 +0300

    Replaced hardcoded colors to variables from colors class

--- MunchkinLevelCounter/UI/Onboarding/OnboardingViewController.swift
+++ MunchkinLevelCounter/UI/Onboarding/OnboardingViewController.swift
@@ -35,20 +35,20 @@ class OnboardingViewController: UIViewController, OnboardingView, EAIntroDelegat
         let pageOne = EAIntroPage()
         pageOne.title = "onboarder.page1.title".localized
         pageOne.desc = "onboarder.page2.description".localized
-        pageOne.titleIconView = UIImageView(image: UIImage(named: "MunchkinLogo"))
-        pageOne.bgColor = UIColor(red:0.97, green:0.77, blue:0.41, alpha:1.0)
+        pageOne.titleIconView = UIImageView(image: UIImage(named: "MunchkinOnboarder"))
+        pageOne.bgColor = Colors.cardGeneral
         
         let pageTwo = EAIntroPage()
         pageTwo.title = "onboarder.page2.title".localized
         pageTwo.desc = "onboarder.page2.description".localized
         pageTwo.titleIconView = UIImageView(image: UIImage(named: "Dice"))
-        pageTwo.bgColor = UIColor(red:0.97, green:0.84, blue:0.45, alpha:1.0)
+        pageTwo.bgColor = Colors.cardLight
     
         let pageThree = EAIntroPage()
         pageThree.title = "onboarder.page3.title".localized
         pageThree.desc  = "onboarder.page3.description".localized
         pageThree.titleIconView = UIImageView(image: UIImage(named: "Infinite"))
-        pageThree.bgColor = UIColor(red:0.23, green:0.09, blue:0.02, alpha:1.0)
+        pageThree.bgColor = Colors.cardCorner
         
         let introView: EAIntroView = EAIntroView(frame: rootView!.bounds, andPages: [pageOne, pageTwo, pageThree])
         introView.backgroundColor = UIColor(colorLiteralRed: 0, green: 0.49, blue: 0.96, alpha: 1)
@@ -58,9 +58,8 @@ class OnboardingViewController: UIViewController, OnboardingView, EAIntroDelegat
     }
     
     func introDidFinish(_ introView: EAIntroView!, wasSkipped: Bool) {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        appDelegate.launchStoryboard(storyboard: Storyboard.Main)
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        appDelegate?.launchStoryboard(storyboard: Storyboard.Main)
     }
-    
     
 }
