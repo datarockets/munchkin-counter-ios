381f3623abf4dc354e870a2f435cc94bca6f25d7
Dzmitry Chyrta <dima@chyrta.com>
2017-03-05 02:26:04 +0300

    Fetching Fabric credentials on-the-fly, updated used Xcode version on Travis, set up caching

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -2,7 +2,8 @@
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   # Travis fetches a shallow clone. We use commit count until HEAD for build number. In order to assure that the count is correct we have to `unshallow` Travis' clone.
   git fetch --unshallow
-
+  echo "$FABRIC_API_TOKEN" >> fabric.apikey
+  echo "$FABRIC_BUILD_SECRET" >> fabric.buildsecret
   fastlane beta
   exit $?
 fi
