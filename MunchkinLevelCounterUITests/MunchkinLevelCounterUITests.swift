d0cbf57a83a5620e75bd1ed9713edb289f0541ce
Pavel Vashkel <vshkl@protonmail.ch>
2017-08-18 12:26:04 +0300

    Take screenshots w/ Fastlane Snapshot tool

--- MunchkinLevelCounterUITests/MunchkinLevelCounterUITests.swift
+++ MunchkinLevelCounterUITests/MunchkinLevelCounterUITests.swift
@@ -12,25 +12,41 @@ class MunchkinLevelCounterUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        continueAfterFailure = false
+
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testScreenshots() {
+        let app = XCUIApplication()
+        XCUIDevice.shared().orientation = .portrait
+        
+        // Players
+        sleep(2)
+        snapshot("0-Launch")
+        
+        // Counter
+        app.navigationBars.buttons.element(boundBy: 0).tap()
+        sleep(2)
+        snapshot("1-Counter")
+        
+        // Results
+        app.navigationBars.buttons.element(boundBy: 1).tap()
+        app.alerts.element(boundBy: 0).buttons.element(boundBy: 1).tap()
+        sleep(2)
+        snapshot("2-Results")
+        
+        // Clean up
+        XCUIApplication().navigationBars.buttons.element(boundBy: 0).tap()
+        
+        XCTAssert(true)
     }
     
 }
